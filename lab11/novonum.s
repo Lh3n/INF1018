15288071058b0078cc285c532d4be54555e316ed
Luiz Henrique Machado da Silva <luizhenriquems2003@gmail.com>
2026-05-16 15:12:03 +0000

    lab11

---
+++ lab11/novonum.s
@@ -0,0 +1,38 @@
+/*
+int novonum(void) {
+  int minhalocal;
+  printf("numero: ");
+  scanf("%d",&minhalocal);
+  return minhalocal;
+}
+*/
+
+/*
+DIC
+...
+
+*/
+
+.text
+.globl novonum
+novonum:
+    pushq   %rbp
+    movq    %rsp, %rbp
+    subq    $16, %rsp
+
+    leaq    msg(%rip), %rdi
+    movl    $0, %eax                # int minhalocal
+    call    printf
+
+    leaq    -4(%rbp), %rsi
+    leaq    fmt(%rip), %rdi
+    movl    $0, %eax
+    call    scanf
+
+    movl    -4(%rbp), %eax
+    leave
+    ret
+
+.section .rodata
+msg:     .string "numero: "
+fmt:     .string "%d"
